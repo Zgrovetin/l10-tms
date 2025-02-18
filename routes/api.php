8f1dabb7934186bb9ca5ec1d9e611695b6ac85a7
Zgrovetin <534477@gmail.com>
2025-02-18 20:28:19 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('project', ProjectController::class);
+Route::apiResource('category', CategoryController::class);
+
+Route::apiResource('project.tasks', TaskController::class)
+->scoped(['task' => 'project']);
+Route::apiResource('category.tasks', TaskController::class)
+->scoped(['task' => 'category']);
+
